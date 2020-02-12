44b0d33deed2df25a7a95482bdb12fa30db22965
krishna <krishnakuma@hcl.com>
2020-02-12 14:46:42 +0530

    book slot

---
+++ src/app/root/components/bookappappointment/bookappappointment.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookappappointmentComponent } from './bookappappointment.component';
+
+describe('BookappappointmentComponent', () => {
+  let component: BookappappointmentComponent;
+  let fixture: ComponentFixture<BookappappointmentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookappappointmentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookappappointmentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
