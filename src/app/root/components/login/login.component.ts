ead435a0b0a20c7efee963e9350beb76246f2929
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-02-12 13:56:14 +0530

    login component

--- src/app/root/components/login/login.component.ts
+++ src/app/root/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm = new FormGroup ({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+  constructor(private formBuilder: FormBuilder, private http: HttpService) { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  // gets login form controls
+  get loginControl() { return this.loginForm.controls; }
+
+  // create a reactive login form
+  createForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(4)]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)]]
+    });
+  }
+
+  // checks whether the user is authorized
+  public login(): void {
+    console.log('loginFrom', this.loginForm.value);
+  }
 }
--- src/app/root/root.module.ts
+++ src/app/root/root.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { RootRoutingModule } from './root-routing.module';
 import { LoginComponent } from './components/login/login.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './services/http.service';
+import { DoctorsViewComponent } from './doctors-view/doctors-view.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, DoctorsViewComponent],
   imports: [
     CommonModule,
-    RootRoutingModule
-  ]
+    RootRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    HttpService
+  ],
+  exports: [LoginComponent]
 })
 export class RootModule { }
